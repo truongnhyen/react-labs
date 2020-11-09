206f3333e4f8758119fea082b341ee71d71d146b
Yen Truong <yentruong.dev@gmail.com>
2020-11-09 21:25:00 +0700

    add emoji to title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Yen Truong</p>
+        <p>Yen Truong 123🤓🤓</p>
         <a
           className="App-link"
           href="https://reactjs.org"
